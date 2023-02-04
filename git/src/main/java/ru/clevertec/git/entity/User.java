1a3e8a5f6a64e8a43772481dfcd1881a9840033b
ArtemAverkov <temaaak@mail.ru>
2023-02-04 13:16:10 +0300

    add: entity/User.java

---
+++ git/src/main/java/ru/clevertec/git/entity/User.java
@@ -0,0 +1,26 @@
+package ru.clevertec.git.entity;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Size(min=2, message = "Не меньше 5 знаков")
+    private String username;
+    @Size(min=2, message = "Не меньше 5 знаков")
+    private String password;
+    private String mail;
+}
